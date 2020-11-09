8d77f5f6f77a3341683cc35215de819f5d304f47
JP <jpjones22@gmail.com>
2020-11-09 12:29:43 -0800

    Spun up server

---
+++ server.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+const env = require('env');
+//const { response } = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.get('/location', handleLocation);
+function handleLocation(req, res) {
+  try {
+    let geoData = require('./data/location.json');
+    let city = req.query.city;
+    let locationData = new Location(city, geoData);
+    res.send(locationData);
+  }
+  catch (error) {
+    console.error(error);
+  }
+}
+function Location(city, geoData) {
+  this.search_query = city;
+  this.formatted_query = geoData[0].display_name;
+  this.latitude = geoData[0].lat;
+  this.longitude = geoData[0].lon;
+}
+
+//404 Handler
+app.use('*', (req, res) => {
+  res.status(404).send('Sorry, that does not exist.  Try again.');
+});
+
+app.listen(PORT, () => {
+  console.log(`server is working on ${PORT}`);
+});
+
